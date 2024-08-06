c5c73588d14f351eca839425e844fdcae5f804cc
RuleBrittonica <mbritton460@gmail.com>
2024-08-06 13:52:57 +1000

    Minor Changes

--- Disk_Utilisation_App/src-tauri/src/filesystem.rs
+++ Disk_Utilisation_App/src-tauri/src/filesystem.rs
@@ -1,4 +1,6 @@
 use std::path::PathBuf;
+use std::fmt;
+
 
 // Struct to represent a file
 #[derive(Debug)]
@@ -21,6 +23,16 @@ impl File {
     }
 }
 
+// Implementing Display for File
+impl fmt::Display for File {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "File: {}\nPath: {:?}\nSize: {} bytes\nPercentage of Root: {:.2}%",
+            self.name, self.path, self.size, self.percentage_of_root
+        )
+    }
+}
 
 // Struct to represent a folder
 #[derive(Debug)]
@@ -69,6 +81,36 @@ impl Folder {
         self.size = self.subfiles.iter().map(|file| file.size).sum::<u64>()
             + self.subfolders.iter().map(|folder| folder.size).sum::<u64>();
     }
+
+    // Method to print the folder hierarchy
+    pub fn print_structure(&self, indent: usize) {
+        println!("{}", self.to_string_with_indent(indent));
+        for subfolder in &self.subfolders {
+            subfolder.print_structure(indent + 2);
+        }
+    }
+
+    // Helper method to format folder information with indentation
+    fn to_string_with_indent(&self, indent: usize) -> String {
+        format!(
+            "{}Folder: {}\n{}Path: {:?}\n{}Size: {} bytes\n{}Percentage of Root: {:.2}%",
+            " ".repeat(indent),
+            self.name,
+            " ".repeat(indent),
+            self.path,
+            " ".repeat(indent),
+            self.size,
+            " ".repeat(indent),
+            self.percentage_of_root
+        )
+    }
+}
+
+// Implementing Display for Folder
+impl fmt::Display for Folder {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.to_string_with_indent(0))
+    }
 }
 
 // Struct to represent the entire disk (root)
@@ -87,29 +129,16 @@ impl Disk {
 
     // Method to update the size and percentage for the entire disk
     pub fn update_disk_size(&mut self) {
-        self.root_folder.update_size();
-        self.update_folder_percentage(&mut self.root_folder, self.root_folder.size);
+        
     }
 
     // Recursive method to calculate folder size
     fn calculate_folder_size(&self, folder: &Folder) -> u64 {
-        let files_size: u64 = folder.subfiles.iter().map(|file| file.size).sum();
-        let subfolders_size: u64 = folder
-            .subfolders
-            .iter()
-            .map(|subfolder| self.calculate_folder_size(subfolder))
-            .sum();
-        files_size + subfolders_size
+        return 0;
     }
 
     // Recursive method to update percentage of each folder and file relative to the root
     fn update_folder_percentage(&self, folder: &mut Folder, root_size: u64) {
-        for file in &mut folder.subfiles {
-            file.percentage_of_root = (file.size as f64 / root_size as f64) * 100.0;
-        }
-        for subfolder in &mut folder.subfolders {
-            subfolder.percentage_of_root = (subfolder.size as f64 / root_size as f64) * 100.0;
-            self.update_folder_percentage(subfolder, root_size);
-        }
+
     }
 }
